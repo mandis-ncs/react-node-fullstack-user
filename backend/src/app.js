9f54b592c9e20ee17908f08be86f612ad19eae98
mandis-ncs <amnd.castro@gmail.com>
2025-02-02 22:44:30 -0300

    add backend doc with swagger

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import setupSwagger from './swagger.js'
 import cors from 'cors'
 import routes from './routes/index.js'
 import errorHandler from './middlewares/errorHandler.js'
 
 const app = express()
+
+setupSwagger(app)
 
 app.use(express.json())
 app.use(cors())
